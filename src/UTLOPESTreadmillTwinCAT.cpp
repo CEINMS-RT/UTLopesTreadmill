9c35e6bf84e2543e2a9aa53ff2a8903a352808c6
Qualisys LOPES <g.v.durandau@utwente.nl>
2023-04-05 17:54:13 +0200

    TC port

--- src/UTLOPESTreadmillTwinCAT.cpp
+++ src/UTLOPESTreadmillTwinCAT.cpp
@@ -24,8 +24,8 @@ void LOPESTreadmillTwinCAT::init(const std::string& subjectName, const std::stri
 	varNameVect_.insert({"l_ground_force_vy", 0});
 	varNameVect_.insert({"ground_force_vy", 0});
 
-	_conversionMap.insert({"l_ground_force_vy", "GaitPhaseDetector.Output.GRF_l_Z"});
-	_conversionMap.insert({"ground_force_vy", "GaitPhaseDetector.Output.GRF_r_Z"});
+	_conversionMap.insert({"l_ground_force_vy", "synergy_speed_control_gait_detector.Output.GRF_l_Z"});
+	_conversionMap.insert({"ground_force_vy", "synergy_speed_control_gait_detector.Output.GRF_r_Z"});
 
 
 	timeStamp_ = rtb::getTime();
